99725ae8b6790a82ac20b92fea0eb9965867206a
mmevorah <mmevorah@umich.edu>
2013-04-06 16:19:02 -0400

    basic controlles
    Map set up behind a controllable car. Going to try the sparrow game
    framework in an attempt to make the car more moveable, and possibly add
    other features.

---
+++ Mevorah Grand Prix/Mevorah Grand Prix/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  Mevorah Grand Prix
+//
+//  Created by Mark Mevorah on 4/5/13.
+//  Copyright (c) 2013 Mark Mevorah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Car : UIView
+
+@end
---
+++ Mevorah Grand Prix/Mevorah Grand Prix/Car.m
@@ -0,0 +1,31 @@
+//
+//  Car.m
+//  Mevorah Grand Prix
+//
+//  Created by Mark Mevorah on 4/5/13.
+//  Copyright (c) 2013 Mark Mevorah. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
